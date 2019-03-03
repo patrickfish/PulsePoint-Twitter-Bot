5f0eb5138b38f9278a536154c6e2af8b6e734bd7
Patrick Fish <patrick_fish@apple.com>
2019-03-03 15:28:35 -0800

    first working version w/ a bunch of debug stuff left

--- bot.py
+++ bot.py
@@ -1,18 +1,17 @@
-from datetime import datetime, timedelta
+#!/usr/bin/env python
+from datetime import datetime
 from pytz import timezone
-from geolocation.main import GoogleMaps
 from credentials import *
 import threading
 import time
-import random
 import requests
+from requests.auth import HTTPBasicAuth
 import tweepy
 
-google_maps = GoogleMaps(api_key='')
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-agencyids = [""]
+agencyids = ["57035"]
 TWEET_SLEEP_TIME = 10
 PP_API_SLEEP = 5
 MINUTES_REMOVE = 5
@@ -26,9 +25,9 @@ def grab_pulsepoint(agencyid):
     while pp_json is None:
         try:
             pp_json = requests.get(url,
-                                   headers=random_headers(),
+                                   auth=HTTPBasicAuth(username, password),
                                    params=(
-                                       ('apikey', ''),
+                                       ('apikey', apikey),
                                        ('agencyid', agencyid),
                                        ('active', '1'),
                                    ),
@@ -47,7 +46,6 @@ def get_print_units(units):
         text = text[:-2]
     return text
 
-
 def check_if_cleared(agencyid):
     threading.Timer(MINUTES_REMOVE * 60, check_if_cleared, args=[agencyid]).start()
     json = grab_pulsepoint(agencyid)
@@ -58,10 +56,13 @@ def check_if_cleared(agencyid):
 
 
 def status_update_pulsepoint(incidentid, calltype, units):
+    print('status_update_pulsepoint()')
     for incident in incidents:
+        print "Printing incident: %s" % incident
         if incident[0] == incidentid:
             if units != incident[2]:
                 try:
+                    print "Updating units"
                     incident[2] = units
                     text = "Units Updated:\n" + get_print_units(units)
                     time.sleep(TWEET_SLEEP_TIME)
@@ -72,6 +73,7 @@ def status_update_pulsepoint(incidentid, calltype, units):
                     print e
             if calltype != incident[1]:
                 try:
+                    print "Updating call type"
                     incident[1] = calltype
                     text = "Call Updated to:\n" + calltype
                     time.sleep(TWEET_SLEEP_TIME)
@@ -83,33 +85,50 @@ def status_update_pulsepoint(incidentid, calltype, units):
 
 
 def loop_update_pulsepoint(agencyid):
+    print "local INCIDENTS: %s" % incidents
+    unit_types = [
+        ('DP', 'DISP'),
+        ('AK', 'ACK'),
+        ('ER', 'ENRT'),
+        ('OS', 'ONSC'),
+        ('TR', 'TRNSPT'),
+        ('TA', 'TRANSPT-A'),
+        ('AQ', 'AVAIL'),
+        ('AR', 'AVAIL'),
+        ('AE', 'AVAIL-OS')
+        ]
+    print('loop_update_pulsepoint')
     threading.Timer(PP_API_SLEEP, loop_update_pulsepoint, args=[agencyid]).start()
     json = grab_pulsepoint(agencyid)
     if json == "" or json['incidents'] is None: return
     for incident in json['incidents']:
+        #print incident
         units = []
         if not 'Unit' in incident: return
         for unit in incident['Unit']:
             units.append([unit['UnitID'], next(
                 fullstatus for (name, fullstatus) in unit_types if name == unit['PulsePointDispatchStatus'])])
-        calltype = next(
-            fullname for (name, fullname) in call_types if name == incident['PulsePointIncidentCallType'])
+        #calltype = next(
+        #    fullname for (name, fullname) in call_types if name == incident['PulsePointIncidentCallType'])
+        calltype = incident['AgencyIncidentCallTypeDescription']
         if not any(incident['ID'] in incident[0] for incident[0] in incidents):
+            print "if not any incidents: %s" % incidents
             try:
                 address = incident['MedicalEmergencyDisplayAddress']
                 dt_obj = datetime.strptime(incident['CallReceivedDateTime'][:-1], "%Y-%m-%dT%H:%M:%S")
                 dt = dt_obj.replace(tzinfo=timezone("UTC")).astimezone(timezone("US/Pacific")).strftime(
                     "%m/%d/%y @ %H:%M:%S %Z")
-                ht = str(address).split(",")[1].replace(" ", "")
-                call = "[" + incident[
-                    'IncidentNumber'] + "]" + " CALL: " + calltype + "\nD/T: " + dt + "\nUNITS: " + get_print_units(
-                    units) + "\nADDR: " + address + "\n#" + ht
+                print dt
+                call = "New Incident: " + calltype + "\nD/T: " + dt + "\nUNITS: " + get_print_units(
+                    units) + "\nADDR: " + address
+                print call
+                print "!!!!!!Tweet is next!!!!!"
                 tweet = api.update_status(call)
                 time.sleep(TWEET_SLEEP_TIME)
                 incidents.append([incident['ID'], calltype, units, tweet.id_str,
                                   dt_obj.replace(tzinfo=timezone("UTC")).astimezone(timezone("US/Pacific")).replace(
                                       tzinfo=None)])
-                print call
+                print "Incident appended"
             except:
                 pass
         else:
--- credentials.py
+++ credentials.py
@@ -1,4 +1,10 @@
+# Twitter credentials
 consumer_key = ''
 consumer_secret = ''
 access_token = ''
 access_token_secret = ''
+
+# PulsePoint credentials
+username = ''
+password = ''
+apikey = ''
